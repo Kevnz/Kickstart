571cbc22a70f312598136c5634675bb64d96d7f5
Kevin Isom <kevin.isom@gmail.com>
2012-02-10 22:10:49 +1300

    fixed to work on windows

--- bin/kickstart.js
+++ bin/kickstart.js
@@ -2,26 +2,30 @@
 
 var sys = require("util"),
 	fs = require('fs'),
+	path = require('path'),
 	KickStart = require("../lib/kickstart"),
 	rootDir = '',
 	fullRoot = '',
-	execRoot = __filename.replace('bin/kickstart.js', '');
+	execRoot =  process.platform === "win32" ? __filename.replace('bin\\kickstart.js', '') : __filename.replace('bin/kickstart.js', '');
 
 var args = process.argv.slice(0);
 // shift off node and script name
 args.shift(); args.shift();
 
 rootDir = args[0];
-fullRoot =process.cwd() + '/' + rootDir;
+console.log(process.cwd());
+console.log('The ExecRoot' + execRoot);
+console.log('__filename ' +__filename);
+fullRoot = path.join(process.cwd() , rootDir);
 fs.mkdirSync(rootDir);
 
 process.chdir(rootDir);
-fs.writeFileSync('package.json', fs.readFileSync(execRoot + '/resources/package.json'));
+fs.writeFileSync('package.json', fs.readFileSync(path.join(execRoot , '/resources/package.json')));
 fs.mkdirSync('views');
 fs.mkdirSync('public');
 fs.mkdirSync('lib');
 
-fs.writeFileSync('app.js', fs.readFileSync(execRoot + '/resources/app.js'));
-fs.writeFileSync('package.json', fs.readFileSync(execRoot + '/resources/package.json'));
+fs.writeFileSync('app.js', fs.readFileSync(path.join(execRoot , '/resources/app.js')));
+fs.writeFileSync('package.json', fs.readFileSync(path.join(execRoot, '/resources/package.json')));
 process.chdir(fullRoot);
 
